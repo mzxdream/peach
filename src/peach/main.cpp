09b3d77292ac250cc8a6d71df664857eeb1d5f55
mengzhaoxiang <mengzhaoxiang@xd.com>
2022-09-28 14:40:21 +0800

    11

--- src/peach/main.cpp
+++ src/peach/main.cpp
@@ -4,6 +4,7 @@
 #include <cmath>
 #include <iostream>
 #include <peach/shader.h>
+#include <stb/stb_image.h>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
@@ -63,17 +64,23 @@ int main()
 
     // set up vertex data (and buffer(s)) and configure vertex attributes
     // ------------------------------------------------------------------
+    // clang-format off
     float vertices[] = {
-        0.5f,
-        -0.5f,
-        0.0f, // bottom right
-        -0.5f,
-        -0.5f,
-        0.0f, // bottom left
-        0.0f,
-        0.5f,
-        0.0f // top
+    //     ---- 位置 ----       ---- 颜色 ----     - 纹理坐标 -
+         0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // 右上
+         0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // 右下
+        -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // 左下
+        -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // 左上
     };
+    unsigned int indices[] = {
+        // 注意索引从0开始! 
+        // 此例的索引(0,1,2,3)就是顶点数组vertices的下标，
+        // 这样可以由下标代表顶点组合成矩形
+
+        0, 1, 3, // 第一个三角形
+        1, 2, 3  // 第二个三角形
+    };
+    // clang-format on
 
     unsigned int VBO, VAO;
     glGenVertexArrays(1, &VAO);
@@ -84,8 +91,19 @@ int main()
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    unsigned int VEO;
+    glGenBuffers(1, &VEO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, VEO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
+
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
 
     // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens.
     // Modifying other VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs)
@@ -96,6 +114,29 @@ int main()
     glBindVertexArray(VAO);
 
     Shader shader("conf/shader/shader.vs", "conf/shader/shader.fs");
+
+    unsigned int texture;
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+    // 为当前绑定的纹理对象设置环绕、过滤方式
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    // 加载并生成纹理
+    int            width, height, nrChannels;
+    unsigned char* data = stbi_load("conf/image/container.jpg", &width, &height, &nrChannels, 0);
+    if (data)
+    {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else
+    {
+        std::cout << "Failed to load texture" << std::endl;
+    }
+    stbi_image_free(data);
+
     // render loop
     // -----------
     while (!glfwWindowShouldClose(window))
@@ -112,14 +153,18 @@ int main()
         // be sure to activate the shader before any calls to glUniform
         shader.use();
 
-        // update shader uniform
-        double timeValue           = glfwGetTime();
-        float  greenValue          = static_cast<float>(sin(timeValue) / 2.0 + 0.5);
-        shader.setVec4("ourColor", 0.0f, greenValue, 0.0f, 1.0f);
-        shader.setFloat("c", t);
+        glBindTexture(GL_TEXTURE_2D, texture);
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
-        // render the triangle
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        // update shader uniform
+        //double timeValue  = glfwGetTime();
+        //float  greenValue = static_cast<float>(sin(timeValue) / 2.0 + 0.5);
+        //shader.setVec4("ourColor", 0.0f, greenValue, 0.0f, 1.0f);
+        //shader.setFloat("c", t);
+
+        //// render the triangle
+        //glDrawArrays(GL_TRIANGLES, 0, 3);
 
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
         // -------------------------------------------------------------------------------
